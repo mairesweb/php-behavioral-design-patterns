3242607d30b9d3d81ef4c2848ea0928dfd152ec6
Marcelo Aires <mairesweb@gmail.com>
2022-07-27 19:49:02 -0300

    feat: design pattern - Template Method.

--- src/Impostos/Ikvp.php
+++ src/Impostos/Ikvp.php
@@ -4,12 +4,19 @@ namespace Alura\DesignPattern\Impostos;
 
 use Alura\DesignPattern\Orcamento;
 
-class Ikvp implements Imposto {
-  public function calculaImposto(Orcamento $orcamento) : float {
-      if ($orcamento->valor > 300 && $orcamento->quantidadeDeItens > 3) {
-        return $orcamento->valor * 0.04;
-      }
+class Ikvp extends ImpostoPara2Aliquotas {
+  protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
+  {
+    return $orcamento->valor > 300 && $orcamento->quantidadeDeItens > 3;
+  }
 
-      return $orcamento->valor * 0.025;
+  protected function calculaTaxaMaxima(Orcamento $orcamento): float
+  {
+    return $orcamento->valor * 0.04;
+  }
+
+  protected function calculaTaxaMinima(Orcamento $orcamento): float
+  {
+    return $orcamento->valor * 0.025;
   }
 }
