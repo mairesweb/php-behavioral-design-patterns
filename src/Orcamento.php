63b09f66c1483bc56bda12641a33ccbe76259715
Marcelo Aires <mairesweb@gmail.com>
2022-07-27 20:00:28 -0300

    feat: adicionando estado ao orçamento.

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -2,7 +2,28 @@
 
 namespace Alura\DesignPattern;
 
+use DomainException;
+
 class Orcamento {
   public int $quantidadeDeItens;
   public float $valor;
+  public string $estadoAtual;
+
+  public function aplicaDescontoExtra() {
+    $this->valor -= $this->calculaDescontoExtra();
+  }
+
+  public function calculaDescontoExtra(): float {
+    if ($this->estadoAtual == 'EM_APROVACAO') {
+      return $this->valor * 0.05;
+    }
+
+    if ($this->estadoAtual == 'AROVADO') {
+      return $this->valor * 0.02;
+    }
+
+    throw new DomainException(
+      'Orçamentos reprovados e finalizados não recebem desconto'
+    );
+  }
 }
