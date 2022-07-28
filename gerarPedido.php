64bec14db6e0dfb27ca519d97b1e777e73108b00
Marcelo Aires <mairesweb@gmail.com>
2022-07-28 13:03:12 -0300

    feat: design pattern - Command.

--- gerarPedido.php
+++ gerarPedido.php
@@ -3,19 +3,11 @@
 require_once "vendor/autoload.php";
 
 use Alura\DesignPattern\{Orcamento, Pedido};
+use Alura\DesignPattern\Comandos\GerarPedido;
 
 $valorOrcamento = $argv[1];
 $numeroDeItens = $argv[2];
 $nomeCliente = $argv[3];
 
-$orcamento = new Orcamento;
-$orcamento->quantidadeDeItens = $numeroDeItens;
-$orcamento->valor = $valorOrcamento;
-
-$pedido = new Pedido;
-$pedido->dataFinalizacao = new \DateTimeImmutable();
-$pedido->nomeCliente = $nomeCliente;
-$pedido->orcamento = $orcamento;
-
-echo 'Cria pedido no banco de dados' . PHP_EOL;
-echo 'Envia e-mail para cliente' . PHP_EOL;
+$gerarPedido = new GerarPedido($valorOrcamento, $numeroDeItens, $nomeCliente);
+$gerarPedido->execute();
---
+++ src/Comandos/GerarPedido.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Alura\DesignPattern\Comandos;
+
+use Alura\DesignPattern\{Orcamento, Pedido};
+use Command;
+
+class GerarPedido implements Command {
+  private float $valorOrcamento;
+  private int $numeroItens;
+  private string $nomeCliente;
+
+  public function __construct(
+    float $valorOrcamento,
+    int $numeroItens,
+    string $nomeCliente
+  ) {
+    $this->valorOrcamento = $valorOrcamento;
+    $this->numeroItens = $numeroItens;
+    $this->nomeCliente = $nomeCliente;
+  }
+
+  public function execute() {
+    $orcamento = new Orcamento();
+    $orcamento->quantidadeDeItens = $this->numeroItens;
+    $orcamento->valor = $this->valorOrcamento;
+
+    $pedido = new Pedido();
+    $pedido->dataFinalizacao = new \DateTimeImmutable();
+    $pedido->nomeCliente = $this->nomeCliente;
+    $pedido->orcamento = $orcamento;
+
+    echo 'Cria pedido no banco de dados' . PHP_EOL;
+    echo 'Envia e-mail para cliente' . PHP_EOL;
+  }
+}
